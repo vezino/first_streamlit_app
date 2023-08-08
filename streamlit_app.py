2cc8d3e10e3eb076b8f4f0bd2e63397129e26f67
vezino <juanmanuelriverom@gmail.com>
2023-08-08 16:15:31 -0600

    Update streamlit_app.py
    add some options

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healty Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Bluberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
