6a59f73f10b7a3987449b95590e15046a302cd0a
vezino <juanmanuelriverom@gmail.com>
2023-08-09 22:21:49 -0600

    Update streamlit_app.py
    fix typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,4 +45,4 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
-streamlit.text_input('Wahat fruit do you like to add','')
+streamlit.text_input('Wahat fruit do you like to add?','')
