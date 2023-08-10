42e9533cdea9212bdb61a8b20f6fa19acfa61ccc
vezino <juanmanuelriverom@gmail.com>
2023-08-10 15:44:50 -0600

    Update streamlit_app.py
    fix typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -74,7 +74,7 @@ if streamlit.button("Get Fruit Load List"):
 #my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values ('from streamlit')")
 
 def insert_row_snowflake(new_fruit):
-  with my_cnx.cursor() as my cur:
+  with my_cnx.cursor() as my_cur:
     my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values ('from streamlit')")
     return "Thanks for adding " + new_fruit
 
